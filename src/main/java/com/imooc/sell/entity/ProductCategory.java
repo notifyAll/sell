5a2388b2acfa6de89ea4d4bf43aa53f961f8b415
NullMoneyException <2281437550@qq.com>
2018-06-06 23:00:49 +0800

    动态插入  insert into product_category (category_name, category_type) values (?, ?)  自动忽略掉两个 时间 null

--- src/main/java/com/imooc/sell/entity/ProductCategory.java
+++ src/main/java/com/imooc/sell/entity/ProductCategory.java
@@ -1,6 +1,7 @@
 package com.imooc.sell.entity;
 
 import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.Entity;
@@ -13,6 +14,7 @@ import java.util.Date;
  * 类目表
  */
 @Entity
+@DynamicInsert  /*动态插入  insert into product_category (category_name, category_type) values (?, ?)  自动忽略掉两个 时间 null*/
 @DynamicUpdate  /* 动态更新注解 数据库中updatetime是自动更新的 如果不用该注解  每次保存都会覆盖  都修改数据没变化时不会update数据*/
 @Data /*lombok 的  可以自动生成get set  甚至tostring*/
 public class ProductCategory {
@@ -25,7 +27,7 @@ public class ProductCategory {
 //    类目编号
     private Integer categoryType;
 
-//    private Date createTime;
-//
-//    private  Date updateTime;
+    private Date createTime;
+
+    private  Date updateTime;
 }
