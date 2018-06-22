f2c2282cf4222542239c2366bdc9816bfbd64b45
NullMoneyException <2281437550@qq.com>
2018-06-22 15:31:50 +0800

    订单相关dao

--- src/main/java/com/imooc/sell/entity/OrderDetail.java
+++ src/main/java/com/imooc/sell/entity/OrderDetail.java
@@ -1,4 +1,46 @@
 package com.imooc.sell.entity;
 
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+
+/**
+ * 订单详情
+ */
+@Entity
+@Data
+@Table(name = "order_detail")
 public class OrderDetail {
+
+    @Id
+    private String detailId;
+    /**
+     * 订单id
+     */
+    private String  orderId;
+    /**
+     * 商品Id
+     */
+    private String productId;
+    /**
+     * 商品名字
+     */
+    private String productName;
+    /**
+     * 单价
+     */
+    private BigDecimal productPrice;
+    /**
+     * 商品数量
+     */
+    private Integer productQuantity;
+    /**
+     * 商品小图
+     */
+    private String productIcon;
 }
--- src/main/java/com/imooc/sell/entity/OrderMaster.java
+++ src/main/java/com/imooc/sell/entity/OrderMaster.java
@@ -1,4 +1,69 @@
 package com.imooc.sell.entity;
 
+import com.imooc.sell.enums.OrderStatusEnum;
+import com.imooc.sell.enums.PayStatusEnum;
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * 订单主表
+ */
+@Entity
+@Table(name = "order_master")
+@Data
+@DynamicUpdate
+@DynamicInsert
 public class OrderMaster {
+
+    /**
+     * 订单id
+     */
+    @Id
+    private String orderId;
+
+    /**
+     * 买家名字
+     */
+    private String buyerName;
+    /**
+     * 买家电话
+     */
+    private String buyerPhone;
+    /**
+     * 买家地址
+     */
+    private String buyerAddress;
+    /**
+     *  买家微信的 openId
+     */
+    private String buyerOpenid;
+
+    /**
+     * 订单总金额
+     */
+    private BigDecimal orderAmmount;
+
+    /**
+     * 订单状态 默认新下单
+     */
+    private Integer orderStatus=OrderStatusEnum.NEW.getCode();
+    /**
+     * 支付状态 默认0 未支付
+     */
+    private Integer payStatus=PayStatusEnum.WAIT.getCode();
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+    /**
+     * 修改时间
+     */
+    private Date updateTime;
 }
