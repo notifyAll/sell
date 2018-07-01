8859b844014c53903e6047b444fac4eef86708f6
NullMoneyException <2281437550@qq.com>
2018-07-01 16:11:44 +0800

    商品实物层添加 加减库存方法

--- src/main/java/com/imooc/sell/service/ProductService.java
+++ src/main/java/com/imooc/sell/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.imooc.sell.service;
 
+import com.imooc.sell.dto.CartDTO;
 import com.imooc.sell.entity.ProductInfo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,15 +16,22 @@ public interface ProductService {
 
     /**
      * 查询所有在架商品列表
+     *
      * @return
      */
-    List<ProductInfo>  findUpAll();
+    List<ProductInfo> findUpAll();
 
     Page<ProductInfo> findAll(Pageable pageable);
 
     ProductInfo save(ProductInfo productInfo);
 
-//    加库存
-    
-//    减库存
+    /**
+     * 加库存
+     */
+    void increaseStock(List<CartDTO> cartDTOList);
+
+    /**
+     * 减库存
+     */
+    void decreaseStock(List<CartDTO> cartDTOList);
 }
--- src/main/java/com/imooc/sell/service/impl/ProductServiceImpl.java
+++ src/main/java/com/imooc/sell/service/impl/ProductServiceImpl.java
@@ -1,12 +1,16 @@
 package com.imooc.sell.service.impl;
 
 import com.imooc.sell.dao.ProductInfoRepository;
+import com.imooc.sell.dto.CartDTO;
 import com.imooc.sell.entity.ProductInfo;
 import com.imooc.sell.enums.ProductStatusEnum;
+import com.imooc.sell.enums.ResultEnum;
+import com.imooc.sell.exception.SellException;
 import com.imooc.sell.service.ProductService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -40,5 +44,38 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public ProductInfo save(ProductInfo productInfo) {
         return repository.save(productInfo);
+    }
+
+    /**
+     * 加库存
+     *
+     * @param cartDTOList
+     */
+    @Override
+    public void increaseStock(List<CartDTO> cartDTOList) {
+
+    }
+
+    /**
+     * 减库存
+     *
+     * @param cartDTOList
+     */
+    @Override
+    @Transactional
+    public void decreaseStock(List<CartDTO> cartDTOList) {
+        for (CartDTO cartDTO:cartDTOList){
+            ProductInfo productInfo = repository.findById(cartDTO.getProductId()).get();
+            if (productInfo==null){
+                throw new SellException(ResultEnum.PRODUCT_NOT_EXIST);
+            }
+
+            Integer result = productInfo.getProductStock() - cartDTO.getProductQuantity();
+            if (result<0){
+                throw  new SellException(ResultEnum.PRODUCT_STOCK_ERROR); //库存异常
+            }
+            productInfo.setProductStock(result);
+            repository.save(productInfo);
+        }
     }
 }
