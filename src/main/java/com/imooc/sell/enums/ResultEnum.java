e62d6a948bb1a2bf0beb656fc7f0a21e2f49700d
NullMoneyException <2281437550@qq.com>
2018-07-11 17:07:37 +0800

    新增一些异常代码

--- src/main/java/com/imooc/sell/enums/ResultEnum.java
+++ src/main/java/com/imooc/sell/enums/ResultEnum.java
@@ -7,6 +7,7 @@ import lombok.Getter;
  */
 @Getter
 public enum ResultEnum {
+    PARAM_ERROR(1,"参数不正确"),
     PRODUCT_NOT_EXIST(10,"商品不存在"),
     PRODUCT_STOCK_ERROR(11,"商品库存不正确"),
     ORDER_NOT_EXIST(12,"订单不存在"),
@@ -14,7 +15,8 @@ public enum ResultEnum {
     ORDER_STATUS_ERROR(14,"订单状态异常"),
     ORDER_UPDATE_FAIL(15,"订单更新失败"),
     ORDER_DETAIL_EMPTY(16,"订单商品详情为空"),
-    PAY_STATUS_ERROR(17,"订单支付状态异常")
+    PAY_STATUS_ERROR(17,"订单支付状态异常"),
+    CART_EMPTY(18,"购物车为空"),
     ;
     private Integer code;
     private String message;
