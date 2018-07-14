7712bb3036ffe2775f9ad741ec122dbdf371b862
NullMoneyException <2281437550@qq.com>
2018-07-14 16:52:40 +0800

    用于 对象转json时  Data 转成Long 前端需要的格式

---
+++ src/main/java/com/imooc/sell/utils/serializer/Date2LongSerializer.java
@@ -0,0 +1,21 @@
+package com.imooc.sell.utils.serializer;
+
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * 用于将对象转换成json 的改变日期的格式  前端需要的
+ */
+public class Date2LongSerializer extends JsonSerializer<Date> {
+
+
+    @Override
+    public void serialize(Date date, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        jsonGenerator.writeNumber(date.getTime()/1000);
+    }
+}
